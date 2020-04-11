06f302ac5f97ecd32d11d94bebee1466a30eeac9
Arun Ramachandriya <Arun.Ramachandriya@investec.co.uk>
2020-04-11 20:24:51 +0100

    Add project files.

---
+++ spf-api/Controllers/CashFlowController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using spf_api.Helpers;
+using SpreadsheetGear;
+
+namespace spf_api.Controllers
+{
+    public class CashFlowController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Calculate(string loanAmount, string interestRate, string term)
+        {
+            //return Ok(new { Message = "Call success"});
+
+            string calcSpreadSheet = new ResourcesHelper().GetAmotizationSpreadSheetFileName();
+
+            IWorkbook workbook = Factory.GetWorkbook(calcSpreadSheet, System.Globalization.CultureInfo.CurrentCulture);
+            IWorksheet worksheet = workbook.Worksheets["Schedule"];
+            IRange cells = worksheet.Cells;
+
+            cells["D6"].Formula = loanAmount;
+            cells["D7"].Formula = interestRate;
+            cells["D8"].Formula = term;
+
+            string totalPayments = cells["H7"].Text;
+            string totalInterest = cells["H8"].Text;
+
+            Dictionary<string, string> retVal = new Dictionary<string, string>();
+            retVal.Add("TotalPayments", totalPayments);
+            retVal.Add("TotalInterest", totalInterest);
+
+
+            File.Delete(calcSpreadSheet);
+
+            return Ok(retVal);
+        }
+    }
+}
---
+++ spf-api/Helpers/ResourcesHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace spf_api.Helpers
+{
+    public class ResourcesHelper
+    {
+        public string GetAmotizationSpreadSheetFileName()
+        {
+            string resourceName = "loan-amortization-schedule";
+
+            Stream fileStream = GetResourceFileStream(resourceName);
+
+            if (fileStream == null) return string.Empty;
+
+            string fileNameWithPath = Path.Combine(Path.GetTempPath(), $"{resourceName}.xlsx");
+
+            if (File.Exists(fileNameWithPath)) File.Delete(fileNameWithPath);
+
+            CopyStream(fileStream, fileNameWithPath);
+
+            return fileNameWithPath;
+        }
+
+        // https://stackoverflow.com/questions/8083473/open-excel-workbook-file-using-embedded-resource
+        private Stream GetResourceFileStream(string fileName)
+        {
+            Assembly currentAssembly = Assembly.GetExecutingAssembly();
+            // Get all embedded resources
+            string[] arrResources = currentAssembly.GetManifestResourceNames();
+
+            foreach (string resourceName in arrResources)
+            {
+                if (resourceName.Contains(fileName))
+                {
+                    return currentAssembly.GetManifestResourceStream(resourceName);
+                }
+            }
+
+            return null;
+        }
+
+        // https://stackoverflow.com/questions/411592/how-do-i-save-a-stream-to-a-file-in-c
+        private void CopyStream(Stream stream, string destPath)
+        {
+            using (var fileStream = new FileStream(destPath, FileMode.Create, FileAccess.Write))
+            {
+                stream.CopyTo(fileStream);
+            }
+        }
+    }
+}
